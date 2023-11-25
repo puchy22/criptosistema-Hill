d3b64ec805f4c84e429eecb535135f4e48db6294
puchy22 <rubendltv22@gmail.com>
2023-11-25 12:12:01 +0100

    Definido el alfabeto close #1 se ha definido el alfabeto que usará el sisetma, además de una función que pasa el valor de una letra a su representación numérica

---
+++ src/alfabeto.py
@@ -0,0 +1,34 @@
+class Alfabeto:
+    def __init__(self):
+        self.alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+        self.traduccion_alfabeto_especial = {
+            "Á": "A",
+            "Ä": "A",
+            "É": "E",
+            "Ë": "E",
+            "Í": "I",
+            "Ï": "I",
+            "Ó": "O",
+            "Ö": "O",
+            "Ú": "U",
+            "Ü": "U",
+            "Ñ": "N",
+        }
+
+    # Datos sobre el alfabeto
+
+    def get_longitud(self):
+        return len(self.alfabeto)
+
+    # Métodos para traducir a valor númerico las letras
+
+    def valor_letra(self, letra: str) -> int | None:
+        letra = letra.upper()
+        if len(letra) == 1:
+            if letra in self.traduccion_alfabeto_especial:
+                return self.alfabeto.index(self.traduccion_alfabeto_especial[letra])
+            else:
+                return self.alfabeto.index(letra)
+        else:
+            return None
