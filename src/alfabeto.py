52f6b1bc4381a9230a94eb17e6fd0840cabdfb5c
puchy22 <rubendltv22@gmail.com>
2023-11-29 01:01:07 +0100

    :passport_control: actualizada visibilidad de atriburos se ha actualizado la visibilidad de los atributos a de la clase, además de añadir una función para devolver la letra dado el valor

--- src/alfabeto.py
+++ src/alfabeto.py
@@ -3,9 +3,9 @@
 
 class Alfabeto:
     def __init__(self):
-        self.alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        self._alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-        self.traduccion_alfabeto_especial = {
+        self._traduccion_alfabeto_especial = {
             "Á": "A",
             "Ä": "A",
             "É": "E",
@@ -19,18 +19,27 @@ class Alfabeto:
             "Ñ": "N",
         }
 
-    def __len__(self):
-        return len(self.alfabeto)
+    def __len__(self) -> int:
+        return len(self._alfabeto)
 
     """Devuelve el valor númerico de una letra del alfabeto"""
 
     def valor_letra(self, letra: str) -> int | None:
         letra = letra.upper()
         if len(letra) == 1:
-            if letra in self.traduccion_alfabeto_especial:
-                return self.alfabeto.index(self.traduccion_alfabeto_especial[letra])
+            if letra in self._traduccion_alfabeto_especial:
+                return self._alfabeto.index(self._traduccion_alfabeto_especial[letra])
             else:
-                return self.alfabeto.index(letra)
+                return self._alfabeto.index(letra)
+        else:
+            return None
+
+    """Devuelve la letra del alfabeto que tiene el valor númerico indicado"""
+
+    def letra_valor(self, valor: int) -> str | None:
+        # Comprueba que el valor esté en el rango del alfabeto
+        if 0 <= valor < len(self._alfabeto):
+            return self._alfabeto[valor]
         else:
             return None
 
@@ -39,9 +48,9 @@ class Alfabeto:
     def validacion(self, letra: str) -> str | None:
         letra = letra.upper()
         if len(letra) == 1:
-            if letra in self.traduccion_alfabeto_especial:
-                return self.traduccion_alfabeto_especial[letra]
-            elif letra in self.alfabeto:
+            if letra in self._traduccion_alfabeto_especial:
+                return self._traduccion_alfabeto_especial[letra]
+            elif letra in self._alfabeto:
                 return letra
             else:
                 return None
