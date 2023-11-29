3df2ac5ed7c4374b2e14199c2287a701948b3df1
puchy22 <rubendltv22@gmail.com>
2023-11-29 01:02:11 +0100

    :necktie: Añadida la lógica de negocio close #8 se ha añadido todo lo necesario para cifrar y descifrar texto

--- src/cripto_hill.py
+++ src/cripto_hill.py
@@ -2,6 +2,7 @@ from .alfabeto import Alfabeto
 from .mensaje import Mensaje
 from sage.matrix.constructor import matrix, random_matrix
 from sage.rings.finite_rings.integer_mod_ring import IntegerModRing
+from sage.arith.misc import GCD
 
 """Clase que representa el criptosistema de Hill"""
 
@@ -15,10 +16,11 @@ class Hill:
             # Generar una matriz aleatoria y no parar hasta que tenga inversa (matriz regular)
             T = random_matrix(self._Z, m, m)
 
-            while not T.is_invertible():
+            while not T.is_invertible() and GCD(T.det(), len(alfabeto)) != 1:
                 T = random_matrix(self._Z, m, m)
 
             self._clave = (m, T)
+            self._alfabeto = alfabeto
         else:
             raise ValueError("m debe ser mayor que 0")
 
@@ -30,10 +32,11 @@ class Hill:
     def hill(self, mensaje: Mensaje, cifrar: bool) -> str:
         mensaje_corregido = str(mensaje)
 
-        # Mientras que el mensaje no tenga el tamaño correcto
-        while (len(mensaje) % self._clave[0]) != 0:
+        # Mientras que el mensaje no tenga el tamaño correcto (múltiplo de m) añadir W al final
+        while (len(mensaje_corregido) % self._clave[0]) != 0:
             mensaje_corregido += "W"
 
+        # Cifrar o descifrar el mensaje en función de la variable cifrar
         if cifrar:
             return self.__traducir(mensaje_corregido, self._clave[1])
         else:
@@ -41,11 +44,34 @@ class Hill:
 
     """Lógica que hace el cifrado de Hill mediante la multiplicación de matrices"""
 
-    def __traducir(self, mensaje: str, matriz: matrix) -> str:
-        for i in range(0, len(mensaje), self._clave[0]):
-            print(mensaje[i : i + self._clave[0]])
+    def __traducir(self, mensaje_orig: str, matriz_clave: matrix) -> str:
+        mensaje_trad = ""
+        for i in range(0, len(mensaje_orig), self._clave[0]):
+            # Crear una matriz columna con los valores de las letras del mensaje_orig
+            M = matrix(
+                self._Z,
+                self._clave[0],
+                1,
+                [
+                    self._alfabeto.valor_letra(letra)
+                    for letra in mensaje_orig[i : i + self._clave[0]]
+                ],
+            )
+            # print(f"Matriz sin traducir:\n{M}")
+            # Multiplicar la matriz columna del mensaje_orig por la matriz de la clave
+            M = matriz_clave * M
 
-        return ""
+            # print(f"Matriz traducida:\n{M}")
+
+            # Pasar la matriz columna con los valores traducidos a letras y añadirlos al mensaje_trad
+
+            for vector in M:
+                for valor in vector:
+                    letra = self._alfabeto.letra_valor(int(valor))
+                    if letra is not None:
+                        mensaje_trad += letra
+
+        return mensaje_trad
 
     """Obtener la clave del criptosistema de Hill"""
 
@@ -59,3 +85,12 @@ class Hill:
             self._clave = (m, random_matrix(self._Z, m, algorithm="unimodular"))
         else:
             raise ValueError("m debe ser mayor que 0")
+
+    """Establecer la clave del criptosistema de Hill, cumpliendo las condiciones de la clave"""
+
+    def set_clave(self, m: int, M: list[list[int]]):
+        T = matrix(self._Z, M)
+        if m > 0 and T.is_invertible() and GCD(T.det(), len(self._alfabeto)) == 1:
+            self._clave = (m, T)
+        else:
+            raise ValueError("m debe ser mayor que 0 y T debe ser regular")
--- tests/cripto_hill_test.py
+++ tests/cripto_hill_test.py
@@ -8,24 +8,43 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from src.alfabeto import Alfabeto
 from src.mensaje import Mensaje
 from src.cripto_hill import Hill
+from math import gcd
 
 
 class TestMensaje(unittest.TestCase):
     def test_clave(self):
         """Comprobar que la clave generada es correcta, es decir,
-        que m es mayor que 0 y t es una matriz m x m invertible (determinante != 0)
+        que m es mayor que 0 y t es una matriz m x m invertible y regular en Z/len(alfabeto)Z
         """
+        alfabeto = Alfabeto()
+        m = 8
+        hill = Hill(m, alfabeto)
+        clave = hill.get_clave()
+        self.assertEqual(clave[0], m)
+        self.assertNotEqual(clave[1], 0)
+        self.assertIs(clave[1].is_invertible(), True)
+        self.assertEqual(gcd(clave[1].det(), len(alfabeto)), 1)
+        # print(f"Matriz identidad:\n{clave[1]*clave[1].inverse()}")
+
+    def test_cifrado_descifrado(self):
+        """Comprobar que al cifrar y descifrar el mismo mensaje da el mismo resultado."""
+
         alfabeto = Alfabeto()
         m = 2
         hill = Hill(m, alfabeto)
-        clave = hill.get_clave()
-        self.assertEqual(clave[0], 2)
-        self.assertNotEqual(clave[1], 0)
-        self.assertIs(clave[1].is_invertible(), True)
+        mensaje = Mensaje(
+            "Hola, esto es una prueba de  que el cifrado funciona correctamente. No multi de tres"
+        )
+        cifrado = hill.hill(mensaje, True)
 
-        men = Mensaje("hola me llamo juan")
+        # Comprobar que el cifrado es distinto al mensaje original
+        self.assertNotEqual(cifrado, mensaje)
 
-        hill.hill(men, True)
+        descifrado = hill.hill(Mensaje(cifrado), False)
+
+        print(f"Mensaje original: {mensaje}")
+        print(f"Mensaje cifrado: {cifrado}")
+        print(f"Mensaje descifrado: {descifrado}")
 
 
 if __name__ == "__main__":
